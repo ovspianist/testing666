8c6e5945c2ca9f983d2138eb823cadf7fdaa9bde
Chuang Niu <ovspianist@gmail.com>
2023-11-07 00:01:12 +0000

    deploy new files

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const titleText = "";
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the NodeSpell test app created by{" "}
-          <code>create-react-app</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {titleText}
-        </a>
+        <div>
+  123
+</div>
+        <div style={{ color: 'gray' }}>Deployed with NodeSpell</div>
       </header>
     </div>
   );
